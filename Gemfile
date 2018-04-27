f5e90f144e11a560c341e1d0159512dc13564c63
Eva <evaliisagalen@gmail.com>
2018-04-26 18:42:26 -0700

    Added database.rb Rake tasks for db

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@
 source "https://rubygems.org"
 
 gem "roda", "~> 3.7.0"
+gem "sequel", "~> 5.7.0"
--- quotes_app.rb
+++ quotes_app.rb
@@ -1,4 +1,10 @@
 require "roda"
+require "sequel"
+
+database = "quotes_development"
+user     = ENV["PGUSER"]
+password = ENV["PGPASSWORD"]
+DB = Sequel.connect(adapter: "postgres", database: database, host: "127.0.0.1", user: user, password: password)
 
 class Quotes < Roda
   plugin :json
