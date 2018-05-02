0aca502011ce4ed7a7deee5df82b685408b6f2c4
Eva <evaliisagalen@gmail.com>
2018-05-01 17:29:14 -0700

    added DELETE route; reconfigures routes to /quotes and /quote/:id

--- quotes_app.rb
+++ quotes_app.rb
@@ -1,38 +1,51 @@
 require "roda"
 require "sequel"
 
-# require "models"
-
 DB = Sequel.connect(adapter: :postgres, database: 'quotes_app', host: 'localhost', user: 'galen')
 
 class Quotes < Roda
   plugin :json
   plugin :json_parser
+  plugin :all_verbs
+  plugin :halt
   require './models/quote.rb'
+
   route do |r|
     r.root do
       {'description'=>'Welcome to quotes API'}
     end
 
-  r.on "quotes" do
-      r.get do
+    r.on "quotes" do
+        r.get do
         #return all quotes
-        @quotes = Quote.all
-        @quotes.map{ |quote| JSON.parse(quote.to_json) }
+          @quotes = Quote.all
+          @quotes.map{ |quote| JSON.parse(quote.to_json) }
+        end
+
+        r.post do
+        #post a new quote
+          @quote = Quote.create(quote_params(r))
+          { quote: JSON.parse(@quote.to_json) }
+        end
       end
 
-      r.get /quotes\/([0-9]+)/ do |id|
-        # return quote by id
-        @quote = Quote[id]
-        { quote: JSON.parse(@quote.to_json) }
+      r.is 'quote', Integer do |quote_id|
+        @quote = Quote[quote_id]
+
+        r.halt(404) unless @quote
+
+        r.get do
+          { quote: JSON.parse(@quote.to_json) }
+        end
+
+        r.delete do
+          @quote.destroy
+          response.status = 200
+          { deleted_quote: JSON.parse(@quote.to_json) }
+        end
       end
 
-      r.post do
-        # post a new quote
-        @quote = Quote.create(quote_params(r))
-        { quote: JSON.parse(@quote.to_json) }
-      end
-    end
+
   end #end of routes
 
   private
